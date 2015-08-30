e0ce565f9ac8986ee4ec0e00275bcf7ac9c983c8
Alberto Francesco Motta <alberto.motta.86@gmail.com>
2015-08-30 19:03:42 +0200

    Added blockrain.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,173 +1,188 @@
 // jshint devel:true
-var container = $('.layer-0');
-if (container.length){
-    var container;
-    var camera, scene, renderer, particles, geometry, materials = [], parameters, i, h, color, size, sprite;
-    var mouseX = 0, mouseY = 0;
+/*global THREE:true, $:true*/
+'use strict';
+var container = $('.starfield');
+var camera;
+var scene;
+var renderer;
+var particles;
+var geometry;
+var materials = [];
+var parameters;
+var i;
+// var h;
+// var color;
+var size;
+// var sprite;
+var mouseX = 0;
+var mouseY = 0;
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
 
-    var windowHalfX = window.innerWidth / 2;
-    var windowHalfY = window.innerHeight / 2;
+function render() {
 
-    init();
-    animate();
+    var time = Date.now() * 0.00005;
 
-    function init() {
+    camera.position.x += ( mouseX - camera.position.x ) * 0.05;
+    camera.position.y += ( -mouseY - camera.position.y ) * 0.05;
 
-        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 3000 );
-        camera.position.z = 1000;
+    camera.lookAt( scene.position );
 
-        scene = new THREE.Scene();
-        scene.fog = new THREE.FogExp2( 0x000000, 0.0007 );
+    for ( i = 0; i < scene.children.length; i++ ) {
 
-        geometry = new THREE.Geometry();
+        var object = scene.children[ i ];
 
-        sprite = THREE.ImageUtils.loadTexture("images/sprite.png");
+        if ( object instanceof THREE.PointCloud ) {
 
-        for ( i = 0; i < 2000; i ++ ) {
-
-            var vertex = new THREE.Vector3();
-            vertex.x = Math.random() * 2000 - 1000;
-            vertex.y = Math.random() * 2000 - 1000;
-            vertex.z = Math.random() * 2000 - 1000;
-
-            geometry.vertices.push( vertex );
-
-        }
-
-        parameters = [
-            [ [1, 1, 0.5], 5 ],
-            [ [0.95, 1, 0.5], 4 ],
-            [ [0.90, 1, 0.5], 3 ],
-            [ [0.85, 1, 0.5], 2 ],
-            [ [0.80, 1, 0.5], 1 ]
-        ];
-
-        for ( i = 0; i < parameters.length; i ++ ) {
-
-            color = parameters[i][0];
-            size  = parameters[i][1];
-
-            materials[i] = new THREE.PointCloudMaterial( { size: size, map: sprite, transparent : true } );
-
-            particles = new THREE.PointCloud( geometry, materials[i] );
-
-            particles.rotation.x = Math.random() * 6;
-            particles.rotation.y = Math.random() * 6;
-            particles.rotation.z = Math.random() * 6;
-
-            scene.add( particles );
-
-        }
-
-        renderer = new THREE.WebGLRenderer();
-        renderer.setPixelRatio( window.devicePixelRatio );
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        renderer.setClearColor( 0xffffff, 1);
-        container.append( renderer.domElement );
-
-        document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-        document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-        document.addEventListener( 'touchmove', onDocumentTouchMove, false );
-
-        //
-
-        window.addEventListener( 'resize', onWindowResize, false );
-
-    }
-
-    function onWindowResize() {
-
-        windowHalfX = window.innerWidth / 2;
-        windowHalfY = window.innerHeight / 2;
-
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-
-        renderer.setSize( window.innerWidth, window.innerHeight );
-
-    }
-
-    function onDocumentMouseMove( event ) {
-
-        mouseX = event.clientX - windowHalfX;
-        mouseY = event.clientY - windowHalfY;
-
-    }
-
-    function onDocumentTouchStart( event ) {
-
-        if ( event.touches.length === 1 ) {
-
-            event.preventDefault();
-
-            mouseX = event.touches[ 0 ].pageX - windowHalfX;
-            mouseY = event.touches[ 0 ].pageY - windowHalfY;
+            object.rotation.y = time * ( i < 4 ? i + 1 : -( i + 1 ) );
 
         }
 
     }
 
-    function onDocumentTouchMove( event ) {
+    // for ( i = 0; i < materials.length; i ++ ) {
+    //
+    //     color = parameters[i][0];
+    //
+    //     h = ( 360 * ( color[0] + time/3 ) % 360 ) / 360;
+    //     materials[i].color.setHSL( h, color[1], color[2] );
+    //
+    // }
 
-        if ( event.touches.length === 1 ) {
+    renderer.render( scene, camera );
+}
 
-            event.preventDefault();
+function onWindowResize() {
 
-            mouseX = event.touches[ 0 ].pageX - windowHalfX;
-            mouseY = event.touches[ 0 ].pageY - windowHalfY;
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
 
-        }
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseMove( event ) {
+
+    mouseX = event.clientX - windowHalfX;
+    mouseY = event.clientY - windowHalfY;
+
+}
+
+function onDocumentTouchStart( event ) {
+
+    if ( event.touches.length === 1 ) {
+
+        event.preventDefault();
+
+        mouseX = event.touches[ 0 ].pageX - windowHalfX;
+        mouseY = event.touches[ 0 ].pageY - windowHalfY;
 
     }
+
+}
+
+function onDocumentTouchMove( event ) {
+
+    if ( event.touches.length === 1 ) {
+
+        event.preventDefault();
+
+        mouseX = event.touches[ 0 ].pageX - windowHalfX;
+        mouseY = event.touches[ 0 ].pageY - windowHalfY;
+
+    }
+
+}
+
+function init() {
+
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 3000 );
+    camera.position.z = 1000;
+
+    scene = new THREE.Scene();
+    scene.fog = new THREE.FogExp2( 0x000000, 0.0007 );
+
+    geometry = new THREE.Geometry();
+
+    // sprite = THREE.ImageUtils.loadTexture("images/sprite.png");
+
+    for ( i = 0; i < 2000; i++ ) {
+
+        var vertex = new THREE.Vector3();
+        vertex.x = Math.random() * 2000 - 1000;
+        vertex.y = Math.random() * 2000 - 1000;
+        vertex.z = Math.random() * 2000 - 1000;
+
+        geometry.vertices.push( vertex );
+
+    }
+
+    parameters = [
+        [ [1, 1, 0.5], 5 ],
+        [ [0.95, 1, 0.5], 4 ],
+        [ [0.90, 1, 0.5], 3 ],
+        [ [0.85, 1, 0.5], 2 ],
+        [ [0.80, 1, 0.5], 1 ]
+    ];
+
+    for ( i = 0; i < parameters.length; i++ ) {
+
+        // color = parameters[i][0];
+        size = parameters[i][1];
+
+        materials[i] = new THREE.PointCloudMaterial( { size: size } );
+        /*, map: sprite, transparent : true*/
+        particles = new THREE.PointCloud( geometry, materials[i] );
+
+        particles.rotation.x = Math.random() * 6;
+        particles.rotation.y = Math.random() * 6;
+        particles.rotation.z = Math.random() * 6;
+
+        scene.add( particles );
+
+    }
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setClearColor( 0x000000, 1);
+    container.append( renderer.domElement );
+
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+    document.addEventListener( 'touchmove', onDocumentTouchMove, false );
 
     //
 
-    function animate() {
+    window.addEventListener( 'resize', onWindowResize, false );
 
-        requestAnimationFrame( animate );
-
-        render();
-
-    }
-
-    function render() {
-
-        var time = Date.now() * 0.00005;
-
-        camera.position.x += ( mouseX - camera.position.x ) * 0.05;
-        camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
-
-        camera.lookAt( scene.position );
-
-        for ( i = 0; i < scene.children.length; i ++ ) {
-
-            var object = scene.children[ i ];
-
-            if ( object instanceof THREE.PointCloud ) {
-
-                object.rotation.y = time * ( i < 4 ? i + 1 : - ( i + 1 ) );
-
-            }
-
-        }
-
-        for ( i = 0; i < materials.length; i ++ ) {
-
-            color = parameters[i][0];
-
-            h = ( 360 * ( color[0] + time/3 ) % 360 ) / 360;
-            materials[i].color.setHSL( h, color[1], color[2] );
-
-        }
-
-        renderer.render( scene, camera );
-
-    }
 }
+
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    render();
+
+}
+
+init();
+animate();
 
 $(document).ready(function() {
     $('#fullpage').fullpage({
         anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
         menu: '#myMenu'
+    });
+    $('.layer-0').blockrain({
+        theme: 'retro',
+        autoplay: true,
+        autoplayRestart: true,
+        speed: 20,
+        autoBlockWidth: true,
+        autoBlockSize: 24
     });
 });
